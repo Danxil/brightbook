e77428ee2691779a44008937f15b3cf374063ef4
danxil <danxil@list.ru>
2015-07-03 13:28:37 +0300

    where_by

--- public/app/filters/maxSymbols.js
+++
@@ -1,8 +0,0 @@
-app.filter("maxSymbols", function() {
-    return function(input, maxLength) {
-        if (input.length > maxLength) {
-            input = input.slice(0, maxLength) + "...";
-        }
-        return input;
-    };
-});
